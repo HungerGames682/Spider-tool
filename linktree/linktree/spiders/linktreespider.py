f8218aea954af075be1aa64dd1350ea31def6a66
Ricker Wilkes <moserdude692@gmail.com>
2022-11-17 08:20:01 -0500

    ahhhhhhhh

--- linktree/linktree/spiders/linktreespider.py
+++ linktree/linktree/spiders/linktreespider.py
@@ -2,21 +2,24 @@ import scrapy
 
 lists = []
 produ = []
+been_to = []
 
 with open('results.txt', 'w') as j:
     j.writelines("")
 
 j.close()
-class AliexpressTabletsSpider(scrapy.Spider):
-    name = 'aliexpress_tablets'
+class LinktreespiderSpider(scrapy.Spider):
+    name = 'linktreespider'
     # allowed_domains = ['pt.youtube.com']
+
+    # Websites to start in
     start_urls = ['http://quotes.toscrape.com/']
 
 
     
     def parse(self, response):
         # Appends the Url list it went to 
-        lists.append(response.url)
+        # lists.append(response.url)
         
         print("procesing:"+response.url)
        
@@ -29,7 +32,15 @@ class AliexpressTabletsSpider(scrapy.Spider):
         complete_url = response.urljoin(further_page_url)
 
         lists.append(complete_url)
-        
+        been_to.append(complete_url)
+
+        for d in range(0, len(lists)):
+            if lists[d] == complete_url:
+                been_to.append("Failure")
+                yield scrapy.Request(complete_url)
+
+            else:
+                yield scrapy.Request(complete_url)
 
         # Writes everything to a file
         for i in range(len(lists)):
